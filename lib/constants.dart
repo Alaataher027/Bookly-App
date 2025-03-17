1741692e3587d9e46d35afe41c7923a0cbea786a
Alaataher027 <alaataher027@gmail.com>
2025-03-17 08:14:30 +0200

    + create app styles + add font

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/widgets.dart';
 
 const kPrimaryColor = Color(0xFF100B20);
 const KTransitionDuration = Duration(microseconds: 250);
+const KGtSectraFine = 'GT Sectra Fine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
@@ -21,7 +23,43 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             "Best Seller",
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4, // width/height
+            child: Container(
+              // width: MediaQuery.of(context).size.width * .38,
+              // height: MediaQuery.of(context).size.height * .27,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.amber,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testBook1,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
           )
         ],
       ),
